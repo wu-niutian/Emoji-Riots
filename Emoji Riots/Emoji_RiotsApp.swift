3d97dd6b802090936caf1272e779906345d086d9
Wu Niutian <wuniutian@gmail.com>
2022-07-23 09:34:36 +0800

    Initial Commit

---
+++ Emoji Riots/Emoji_RiotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_RiotsApp.swift
+//  Emoji Riots
+//
+//  Created by    wuniutian on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_RiotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
